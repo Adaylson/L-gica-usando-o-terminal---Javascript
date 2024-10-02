74c9e7d812c8814a76f79f90787002d8041efabe
Adaylson Arcanjo <Adaylsonevaristo15@gmail.com>
2024-10-02 16:24:30 -0300

    02/10

--- Faccat/idadeT.js
+++ Faccat/idadeT.js
@@ -4,11 +4,25 @@ const entrada = require('prompt-sync')();
 
 console.log('Bem vindo a minha função')
 let cont = 1 
-while(cont == i){
+while(cont == 1){
     let ano = parseInt(entrada("Digite o seu ano de nascimento: "));
-    let mes = parseInt(entrada('Digite o mes de seu nascimento'));
-    let dia = parseInt(entrada('Digite o dia de seu nascimento'));
+    let mes = parseInt(entrada('Digite o mes de seu nascimento: '));
+    let dia = parseInt(entrada('Digite o dia de seu nascimento: '));
 
-    let data = new Date;
+    let dataa = Date.now();
+    let data = new Date(dataa)
+    let diaA = data.getDay()
+    let mesA = data.getMonth()
+    let anoA = data.getFullYear()
 
+    let tempoVida = 365*(anoA-ano)+30*(mesA-mes)+(diaA-dia);
+    
+    console.log("O seu tempo de vida em dias, é equivalente a:", tempoVida," dias")
+
+    console.log("Função finalizada com sucesso!")
+    console.log("Deseja fazer novamente?")
+    cont = parseInt(entrada("[1]sim [0]não: "))
 }
+
+console.log("Função finalizada com sucesso!")
+console.log("Até mais tarde :)")
